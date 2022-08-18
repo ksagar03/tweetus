c8e91193502ddcb25ba666654114c5302cc3c171
ksagar03 <sagar22krishna03@gmail.com>
2022-08-18 21:22:13 +0530

    added functionality to like btn

--- tweetus-web/src/components/Buttons.js
+++ tweetus-web/src/components/Buttons.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Button({action,style,tweet}) {
     const className = style ? style : 'btn btn-dark'
-    let like =tweet.likes
+    const  [like,setLike] =useState(tweet.likes ? tweet.likes : 0)
+    const [onsecondtimeclick,setOnsecondtimeclick]= useState(false)
     const actionsdisplay = action.display ? action.display : 'Action'
     const display =action.type === "Likes" ? `${like} ${actionsdisplay}` : `${actionsdisplay}`
     const handleonClick =(ev) =>{
       ev.preventDefault()
       if(action.type ==='Likes'){
-        console.log(tweet.likes + 1)
-        like=tweet.likes +1
+        if(onsecondtimeclick === true){
+          setLike(like -1)
+          setOnsecondtimeclick(false)
+        }else
+        {
+          setLike(tweet.likes +1)// here using useState we are changing updating the likes
+          setOnsecondtimeclick(true)
+        }
+        
+
+        // like=tweet.likes +1 // this line will not save the values because in function we need to use
+        //hooks (useState) to save the changes(and also to display)   
       }
     }
     return (
